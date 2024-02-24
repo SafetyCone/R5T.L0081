73a024c18dccd3f19ed7d2df7b0b5853c26c9176
davidcoats <david@getsafetycone.com>
2024-02-24 00:43:55 -0800

    Interim changes.

--- source/R5T.L0081.O002/Code/Values/IGitHubClientContextOperations.cs
+++ source/R5T.L0081.O002/Code/Values/IGitHubClientContextOperations.cs
@@ -3,7 +3,9 @@ using System.Threading.Tasks;
 
 using R5T.L0078.T000;
 using R5T.L0079;
+using R5T.L0091.T000;
 using R5T.T0131;
+using R5T.T0221;
 using R5T.T0235.T000;
 
 
@@ -21,6 +23,66 @@ namespace R5T.L0081.O002
     public partial interface IGitHubClientContextOperations : IValuesMarker,
         L0078.O001.IGitHubClientContextOperations
     {
+        /// <inheritdoc cref="IGitHubOperator.Create_Repository_NonIdempotent(Octokit.GitHubClient, RepositorySpecification)"/>
+        public Func<TRepositoryContext, Task> Create_Repository_NonIdempotent<TRepositoryContext, TGitHubContext>(
+            TGitHubContext gitHubContext,
+            (IsSet<IHasGitHubClient> GitHubClientSet, IsSet<IHasRepositorySpecification> RepositorySpecificiationSet) propertiesRequired,
+            out IsSet<IHasRepository> propertiesSet,
+            out IChecked<IGitHubRepositoryExists> @checked)
+            where TGitHubContext:
+            IHasGitHubClient
+            where TRepositoryContext :
+            IWithRepository,
+            IHasRepositorySpecification
+        {
+            @checked = Checked.Check<IGitHubRepositoryExists>();
+
+            return async context =>
+            {
+                context.Repository = await Instances.GitHubOperator.Create_Repository_NonIdempotent(
+                    gitHubContext.GitHubClient,
+                    context.RepositorySpecification);
+            };
+        }
+
+        /// <inheritdoc cref="IGitHubOperator.Create_Repository_NonIdempotent(Octokit.GitHubClient, RepositorySpecification)"/>
+        public Func<TRepositoryContext, TGitHubContext, Task> Create_Repository_NonIdempotent<TRepositoryContext, TGitHubContext>(
+            (IsSet<IHasGitHubClient> GitHubClientSet, IsSet<IHasRepositorySpecification> RepositorySpecificiationSet) propertiesRequired,
+            out IsSet<IHasRepository> propertiesSet,
+            out IChecked<IGitHubRepositoryExists> @checked)
+            where TGitHubContext :
+            IHasGitHubClient
+            where TRepositoryContext :
+            IWithRepository,
+            IHasRepositorySpecification
+        {
+            @checked = Checked.Check<IGitHubRepositoryExists>();
+
+            return async (repositoryContext, gitHubContext) =>
+            {
+                repositoryContext.Repository = await Instances.GitHubOperator.Create_Repository_NonIdempotent(
+                    gitHubContext.GitHubClient,
+                    repositoryContext.RepositorySpecification);
+            };
+        }
+
+        /// <inheritdoc cref="IGitHubOperator.Create_Repository_NonIdempotent(Octokit.GitHubClient, RepositorySpecification)"/>
+        public Func<TContext, Task> Create_Repository_NonIdempotent<TContext>(
+            (IsSet<IHasGitHubClient> GitHubClientSet, IsSet<IHasRepositorySpecification> RepositorySpecificiationSet) propertiesRequired,
+            out IsSet<IHasRepository> propertiesSet,
+            out IChecked<IGitHubRepositoryExists> @checked)
+            where TContext : IHasGitHubClient, IWithRepository, IHasRepositorySpecification
+        {
+            @checked = Checked.Check<IGitHubRepositoryExists>();
+
+            return async context =>
+            {
+                context.Repository = await Instances.GitHubOperator.Create_Repository_NonIdempotent(
+                    context.GitHubClient,
+                    context.RepositorySpecification);
+            };
+        }
+
         /// <inheritdoc cref="IGitHubOperator.Create_Repository_NonIdempotent(Octokit.GitHubClient, RepositorySpecification)"/>
         public Func<TContext, Task> Create_Repository_NonIdempotent_I01<TContext>(
             RepositorySpecification repositorySpecification)
@@ -55,6 +117,53 @@ namespace R5T.L0081.O002
                 context.GitHubClient,
                 context.RepositoryOwnerName,
                 context.RepositoryName);
+        }
+
+        /// <inheritdoc cref="L0078.F001.IGitHubClientOperator.Delete_Repository_Idempotent(Octokit.GitHubClient, string, string)"/>
+        public Func<TRepositoryContext, Task> Delete_Repository_Idempotent<TRepositoryContext, TGitHubContext>(
+            TGitHubContext gitHubContext,
+            Func<TRepositoryContext, bool, Task> outputHandler = default)
+            where TGitHubContext :
+            IHasGitHubClient
+            where TRepositoryContext : 
+            IHasRepositoryOwnerName,
+            IHasRepositoryName
+        {
+            return async context =>
+            {
+                var wasDeleted = await Instances.GitHubClientOperator.Delete_Repository_Idempotent(
+                    gitHubContext.GitHubClient,
+                    context.RepositoryOwnerName,
+                    context.RepositoryName);
+
+                await Instances.FunctionOperator.Run(
+                    context,
+                    wasDeleted,
+                    outputHandler);
+            };
+        }
+
+        /// <inheritdoc cref="L0078.F001.IGitHubClientOperator.Delete_Repository_Idempotent(Octokit.GitHubClient, string, string)"/>
+        public Func<TRepositoryContext, TGitHubContext, Task> Delete_Repository_Idempotent<TRepositoryContext, TGitHubContext>(
+            Func<TRepositoryContext, bool, Task> outputHandler = default)
+            where TGitHubContext :
+            IHasGitHubClient
+            where TRepositoryContext :
+            IHasRepositoryOwnerName,
+            IHasRepositoryName
+        {
+            return async (repositoryContext, gitHubContext) =>
+            {
+                var wasDeleted = await Instances.GitHubClientOperator.Delete_Repository_Idempotent(
+                    gitHubContext.GitHubClient,
+                    repositoryContext.RepositoryOwnerName,
+                    repositoryContext.RepositoryName);
+
+                await Instances.FunctionOperator.Run(
+                    repositoryContext,
+                    wasDeleted,
+                    outputHandler);
+            };
         }
 
         /// <inheritdoc cref="L0078.F001.IGitHubClientOperator.Delete_Repository_Idempotent(Octokit.GitHubClient, string, string)"/>
---
+++ source/R5T.L0081.O002/Code/_Types/Checks/IGitHubRepositoryExists.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.T0244;
+
+
+namespace R5T.L0081.O002
+{
+    [CheckMarker]
+    public interface IGitHubRepositoryExists : ICheckMarker
+    {
+    }
+}
